b0a9b9b54abd28f4d72f6014766bdb7615b04bee
Papryq <piq1998pll@wp.pl>
2023-08-25 20:40:30 +0200

    Connected supabase to the project, created providers and userUser hook which checks the session of the user, details and subscription status

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
-import Sidebar from '@/components/Sidebar'
 import './globals.css'
-import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+
+
+import Sidebar from '@/components/Sidebar'
+import type { Metadata } from 'next'
+import SupabaseProvider from '@/providers/SupabaseProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -18,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <Sidebar>
+            {children}
+          </Sidebar>
+        </SupabaseProvider>
         </body>
     </html>
   )
